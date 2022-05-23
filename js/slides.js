3ed4b294eab8f80167320f3f566efe65eece187d
JoaoGabrielOliveira <joaogabrieloliveira81@gmail.com>
2022-05-23 14:21:31 -0300

    Change lorem Ipsum to Mussum Ipsum

--- js/slides.js
+++ js/slides.js
@@ -1,7 +1,7 @@
 const slides = [
-    slide("Natural Environment", 2027, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum viverra nunc tincidunt est feugiat tincidunt.", "backgrounds/background1.jpg"),
-    slide("Family House", 2025, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum viverra nunc tincidunt est feugiat tincidunt.", "backgrounds/background2.jpg"),
-    slide("Modern Architecture", 2026, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum viverra nunc tincidunt est feugiat tincidunt.", "backgrounds/background3.jpg")
+    slide("Natural Environment", 2027, "Mussum Ipsum, cacilds vidis litro abertis. Sapien in monti palavris qui num significa nadis i pareci latim.Quem num gosta di mé, boa gentis num é.Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.", "backgrounds/background1.jpg"),
+    slide("Family House", 2025, "Quem manda na minha terra sou euzis!Suco de cevadiss deixa as pessoas mais interessantis.Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis.Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis.", "backgrounds/background2.jpg"),
+    slide("Modern Architecture", 2026, "Si num tem leite então bota uma pinga aí cumpadi!Viva Forevis aptent taciti sociosqu ad litora torquent.Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!", "backgrounds/background3.jpg")
 ]
 
 function slide(title, date, text, img){
