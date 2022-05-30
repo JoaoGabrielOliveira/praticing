dc861a140e66f9f014514614cd494b8298a7f5ab
JoaoGabrielOliveira <joaogabrieloliveira81@gmail.com>
2022-05-30 19:25:21 -0300

    Using templete

--- main.go
+++ main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
+
+type Car struct {
+	Id       string
+	Model_id int
+	Price    float64
+}
+
+var temp = template.Must(template.ParseGlob("templetes/*.html"))
 
 func main() {
 	startApp()
 }
 
 func startApp() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", index)
 	http.ListenAndServe(":8090", nil)
 }
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
+func index(w http.ResponseWriter, req *http.Request) {
+
+	cars := []Car{
+		{"asd-asd", 1, 20000}, {"dsa-dsa", 1, 20000}, {"sad-sad", 1, 20000}}
+
+	err := temp.ExecuteTemplate(w, "Index", cars)
+
+	fmt.Println(err)
 }
