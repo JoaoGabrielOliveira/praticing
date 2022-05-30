fd44fa987ed5f22da8076ba93a777f51d430c9b8
JoaoGabrielOliveira <joaogabrieloliveira81@gmail.com>
2022-05-29 21:05:07 -0300

    Doing hello world with server

---
+++ app.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	startApp()
+}
+
+func startApp() {
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8090", nil)
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
