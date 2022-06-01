100a08f3fb3c3d25637a1a567d7e810fb42d0eda
JoaoGabrielOliveira <joaogabrieloliveira81@gmail.com>
2022-06-01 14:53:30 -0300

    Init mod in project Add connection with database

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/JoaoGabrielOliveira/praticing/go/web-server
+
+go 1.18
+
+require github.com/lib/pq v1.10.6 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 type Car struct {
@@ -20,15 +23,42 @@ func main() {
 
 func startApp() {
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8090", nil)
+	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
+	db := connect_database("host=localhost port=5432 user=postgres dbname=postgres password=1234 sslmode=disable")
 
-	cars := []Car{
-		{"asd-asd", 1, 20000}, {"dsa-dsa", 1, 20000}, {"sad-sad", 1, 20000}}
+	car := Car{}
+	cars := []Car{}
 
-	err := temp.ExecuteTemplate(w, "Index", cars)
+	cars_rows, err := db.Query("SELECT * FROM cars")
+	for cars_rows.Next() {
+		var Id string
+		var Model_id int
+		var Price float64
+
+		row_error := cars_rows.Scan(&Id, &Model_id, &Price)
+		if row_error != nil {
+			panic(row_error.Error())
+		}
+
+		car.Id = Id
+		car.Model_id = Model_id
+		car.Price = Price
+
+		cars = append(cars, car)
+	}
+	err = temp.ExecuteTemplate(w, "Index", cars)
+	defer db.Close()
 
 	fmt.Println(err)
+}
+
+func connect_database(conn string) *sql.DB {
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
 }
