ec65cc6c631447beeb5277d24b0cf55ee1615657
JoaoGabrielOliveira <joaogabrieloliveira81@gmail.com>
2022-06-03 10:31:35 -0300

    Add Car page

--- router/Router.go
+++ router/Router.go
@@ -6,16 +6,16 @@ import (
 	"html/template"
 	"net/http"
 	"praticing/models"
-	"praticing/repository/car"
+	CarRepository "praticing/repository/car"
 	"praticing/util"
 )
 
 var temp = template.Must(template.ParseGlob("views/*.html"))
 
-var Routes = make(map[string]http.HandlerFunc)
+var Routes = map[string]http.HandlerFunc{
+	"/": Index, "/car": Car}
 
 func StartApp() {
-	Routes["/"] = Index
 
 	for path, route := range Routes {
 		http.HandleFunc(path, route)
@@ -28,11 +28,30 @@ func Index(w http.ResponseWriter, req *http.Request) {
 	var err error
 	switch req.Method {
 	case http.MethodGet:
-		err = temp.ExecuteTemplate(w, "Index", car.All())
+		err = temp.ExecuteTemplate(w, "Index", CarRepository.All())
+	}
+
+	util.CheckError(err)
+}
+
+func Car(w http.ResponseWriter, req *http.Request) {
+	var err error
+	switch req.Method {
+	case http.MethodGet:
+		id, ok := req.URL.Query()["id"]
+		util.CheckError(err)
+
+		if !ok || len(id[0]) < 1 {
+			fmt.Println("Url Param 'key' is missing")
+			return
+		} else {
+			err = temp.ExecuteTemplate(w, "Car", CarRepository.Get(id[0]))
+		}
+
 	case http.MethodPost:
 		c := models.Car{}
 
-		json.NewDecoder(req.Body).Decode(&c)
+		err = json.NewDecoder(req.Body).Decode(&c)
 		fmt.Fprintf(w, "Car: %+v", c)
 	}
 
