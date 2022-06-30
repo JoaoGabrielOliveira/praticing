daf16c6edffd34f2071bd5c6108619ea7d1dd5d2
JoaoGabrielOliveira <joaogabrieloliveira81@gmail.com>
2022-06-30 11:55:21 -0300

    Add migrations (NOT TESTED)

---
+++ database/Migrations/Course.sql
@@ -0,0 +1,5 @@
+CREATE TABLE course (
+    id INT PRIMARY KEY,
+    code CHAR(25) NOT NULL,
+    title TEXT NOT NULL
+)
---
+++ database/Migrations/Student.sql
@@ -0,0 +1,7 @@
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    firstName VARCHAR(60) NOT NULL,
+    lastName VARCHAR(60) NOT NULL,
+    registrationNumber VARCHAR(15) NOT NULL
+
+)
