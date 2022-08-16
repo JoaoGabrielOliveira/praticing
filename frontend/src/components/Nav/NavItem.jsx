ba3f1088c75be0547eb71b6880322e71f6f73ea2
joao.conceicao <joaogabrieloliveira81@gmail.com>
2022-08-16 09:59:16 -0300

    Implement NavCollection Improve Nav and style

--- frontend/src/components/Nav/NavItem.jsx
+++ frontend/src/components/Nav/NavItem.jsx
@@ -1,30 +1,10 @@
 import React, {useState} from 'react';
 
     function NavItem(props){
-    const [showOptions, setShowOptions] = useState(false)
-    
-    const handleShowOptions = () => {
-        console.info("State is True");
-        setShowOptions(true)
-    }
-
-    const handleCloseWhenBlur = () => {
-        alert("Saiu de foco")
-        setShowOptions(false)
-    }
-
-    let items;
-    if (props.items?.length > 0){
-        items = <ul>{props.items.map(element =>
-                <li key={element.key}>{element}</li>
-            )}</ul>;
-    }
-
 
     return (
         <button onClick={props.onClick}>
             <label>{props.label}</label>
-            {showOptions ? items : ""}
         </button>
     )
 }
--- frontend/src/components/Nav/index.jsx
+++ frontend/src/components/Nav/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import NavCollection from './NavCollection';
 import NavItem from './NavItem';
 import "./style.css";
 
+/*
+*/
+//<NavItem label="Editar" />
+//<NavItem label="Sobre" />
+
 export default () => <nav>
-    <NavItem label="Arquivo" />
-    <NavItem label="Editar" />
-    <NavItem label="Sobre" />
+    <NavCollection label="Arquivo">
+        <NavItem label="Criar novo arquivo" onClick={() => alert("Novo")}/>
+        <NavItem label="Abrir arquivo" onClick={() => alert("Abrir")}/>
+        <NavItem label="Salvar arquivo" onClick={() => alert("Salvar")}/>
+    </NavCollection>
 </nav>
