4d91e4387c7366010655f409527e4cb8e8b38869
= <=>
2022-08-22 00:09:54 -0300

    Add save file operation

--- app.go
+++ app.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 // App struct
@@ -24,4 +27,33 @@ func (a *App) startup(ctx context.Context) {
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+func (a *App) SaveNote(text string) {
+	selection, err := runtime.SaveFileDialog(a.ctx, runtime.SaveDialogOptions{
+		Title:           "Salvando documento",
+		DefaultFilename: ".txt",
+		Filters: []runtime.FileFilter{
+			{
+				DisplayName: "Texto (*.txt",
+				Pattern:     "*.txt",
+			}, {
+				DisplayName: "Todos (*.*",
+				Pattern:     "*.*",
+			},
+		},
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	if len(selection) == 0 {
+		runtime.LogInfo(a.ctx, "Operation was canceled")
+		return
+	}
+
+	d := []byte(text)
+	os.WriteFile(selection, d, 0644)
+	runtime.LogInfo(a.ctx, "A file as saved: "+selection)
 }
